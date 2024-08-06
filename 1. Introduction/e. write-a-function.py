c43c16384b7c248bbd2cb14382c428fc7cea6fdc
Gabriel-Okom <ougabriel@gmail.com>
2024-08-06 02:42:47 +0100

    Added files and folders

---
+++ 1. Introduction/e. write-a-function.py
@@ -0,0 +1,64 @@
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    #A leap year is divisible by 4 only
+    #a leap year is divisible by 4 and not by 100
+    #a century leap year is divisible by 400 and by 100
+    if year % 4 == 0 and year % 100 != 0:
+        leap = True
+        
+    if year % 400 == 0 and year % 100 == 0:
+            leap = True
+     
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
+
+
+
+################################################
+###############################################
+
+# Method 2
+
+def is_leap(year):
+    leap = False
+    leap2 = True
+    # Write your logic here
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return leap2
+            else:
+                return leap
+        else:
+            return leap2
+    else:
+         return leap
+
+year = int(input())
+print(is_leap(year))
+
+
+
+# An extra day is added to the calendar almost every four years as February 29, and the day is called a leap day. It corrects the calendar for the fact that our planet takes approximately 365.25 days to orbit the sun. A leap year contains a leap day.
+
+# In the Gregorian calendar, three conditions are used to identify leap years:
+
+# The year can be evenly divided by 4, is a leap year, unless:
+# The year can be evenly divided by 100, it is NOT a leap year, unless:
+# The year is also evenly divisible by 400. Then it is a leap year.
+# This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years. Source
+
+# Task
+
+# Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
+
+# Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
+
+# Input Format
+
+# Read , the year to test.
